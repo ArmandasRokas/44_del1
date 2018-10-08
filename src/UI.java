5d1a9eeaff2ff9f4e9efdd5e765588bdc908be4d
Armandas <armandas.rokas@gmail.com>
2018-10-08 12:14:05 +0200

    First commit

---
+++ src/UI.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class UI {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter 1 for Roll die");
+
+    }
+}
