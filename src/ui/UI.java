a42259eba8cfc495500ee1012c454a8bc93906d2
Armandas <armandas.rokas@gmail.com>
2018-10-08 12:27:42 +0200

    Game instant is made

--- src/ui/UI.java
+++ src/ui/UI.java
@@ -1,13 +1,19 @@
 package ui;
 
+import controller.Game;
+
 import java.util.Scanner;
 
 public class UI {
 
     public static void main(String[] args) {
+
+        Game game = new Game();
+
         System.out.println("Test");
         Scanner s = new Scanner(System.in);
         System.out.println("Enter 1 for Roll die");
+
 
     }
 }
