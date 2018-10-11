c3fb72aaf3c2d60cb8f6b02ea2f22ffa7121c6cc
simon <simonphilipsen1990@gmail.com>
2018-10-11 15:00:15 +0200

    Torsdags arbejde

--- src/model/Die.java
+++ src/model/Die.java
@@ -1,18 +1,34 @@
 package model;
 
+/**@author  Hold 44
+ * @version 11/10-2018
+ *
+ * Defines the Die class
+ */
 public class Die {
-    //Variables
-    private int eyes;
+    private int eyes;   //Eyes of the dice
 
-    //Constructor
+    /**
+     * Constructor of the Die
+     */
     public Die(){
        this.eyes = 1;
     }
 
-    //Methods
+    /**
+     * Get method for Eyes
+     *
+     * @return Value of Eyes
+     */
     public int getEyes() {
         return eyes;
     }
+
+    /**
+     * Checks if the Eyes have the Value between 1 and 6
+     *
+     * @param eyes If they aren't within range of 1 and 6, it will print out and error
+     */
     public void setEyes(int eyes) {
         if(1 <= eyes && eyes <= 6) {
             this.eyes = eyes;
