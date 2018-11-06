daf7a3327c85bcc5a925bbaeaab51c17388035dd
Kristian Andersen <kristian1711@gmail.com>
2018-11-06 11:56:49 +0100

    accountNegativeTotalCashTest() created in AccountTest class

--- test/model/AccountTest.java
+++ test/model/AccountTest.java
@@ -10,9 +10,24 @@ class AccountTest {
     /**
      * Test case: TC###
      *
-     * Tests that it should not be possible to for a Player's Account, totalCash
+     * Tests that it should not be possible to for a Player's Account's totalCash to be negative after a turn has been played.
+     * If it is negative, it should be changed to 0 instead.
      */
     @Test
-    void addToCash() {
+    void accountNegativeTotalCashTest() {
+        Player testPlayer = new Player("testNegativeCash");
+        int startingCash = testPlayer.getTotalCash();
+
+        testPlayer.addToCash(-999);
+        int cashAfterNegativeInput = testPlayer.getTotalCash();
+        testPlayer.addToCash(-1);
+        int cashAtZero = testPlayer.getTotalCash();
+        testPlayer.addToCash(-10);
+        int cashAfterNegativeValue = testPlayer.getTotalCash();
+
+        assertEquals(1000, startingCash);
+        assertEquals(1, cashAfterNegativeInput);
+        assertEquals(0, cashAtZero);
+        assertEquals(0, cashAfterNegativeValue);
     }
 }
