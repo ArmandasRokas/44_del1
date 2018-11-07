8df2dc400cd15acd94b3b68fb2c87a799c3a30f9
Armandas <armandas.rokas@gmail.com>
2018-11-07 16:30:19 +0100

    Added description to readFromFile service

--- src/util/GameTool.java
+++ src/util/GameTool.java
@@ -23,23 +23,30 @@ public class GameTool {
     }
 
 
-
+    /**
+     *  This method reads a text file.
+     *
+     * @param filename pure name without location or .txt
+     * @return an array list with elements which are representing each line in the text file.
+     * @throws IOException
+     */
     public static ArrayList<String> readFromFile(String filename) throws IOException{
 
-        ArrayList<String> list = new ArrayList<String>();
-        list.add("spacing....."); // add an element to 0 index
+        ArrayList<String> values = new ArrayList<String>();  // declaring arraylist with name "values"
+        values.add("spacing.....");   // add an element to 0 index, so element in index 1 is the same element
+                                      // in the list in line number 1. It makes just easier to manage txt file.
 
-        String file ="languages\\"+filename+".txt";
+        String file ="languages\\"+filename+".txt"; // inserting filename parameter to the whole file name declaration
 
         BufferedReader reader = new BufferedReader(new FileReader(file));
-        String currentLine = reader.readLine();
-        while (currentLine != null){
-            list.add(currentLine);
-            currentLine = reader.readLine();
+        String currentLine = reader.readLine();  // reads a line
+        while (currentLine != null){ // runs loop until the current line is not empty.
+            values.add(currentLine);   // add line as an element to the array list
+            currentLine = reader.readLine();  // reads next line
         }
         reader.close();
 
 
-        return list;
+        return values;
     }
 }
