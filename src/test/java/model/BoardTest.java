ecc5c1c37de7a234a20ed1e259bddc34d0a70056
Armandas <armandas.rokas@gmail.com>
2018-11-19 17:08:13 +0100

    Added comment to Test case 5

--- src/test/java/model/BoardTest.java
+++ src/test/java/model/BoardTest.java
@@ -5,8 +5,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Testing
+/**Test case: TC5
+ *
+ * Tests if a new position calculated by the getNewPosition method
+ * in the Board class is not extending the number of squares.
+ * In other words, the method starts counting position from
+ * the beginning, if the end of squares reaches.
  *
  */
 
@@ -14,30 +18,20 @@ class BoardTest {
 
     @Test
     void getNewPositionTest(){
-
         //Arrange
-
         Board board = new Board(12);
-
         int boardLength = board.getLength();
         int startPosition = 0;
         int expAfterMovingToLastSquare = boardLength - 1;
         int expAfterMovingOneRound = 0;
 
-
         //Act
-
-
         int newPositionAfterMovingToLastSquare = board.getNewPosition(startPosition, boardLength-1);
         int newPositionAfterMovingOneRound = board.getNewPosition(boardLength - 1, 1);
 
-
         //Assert
-
         assertEquals(expAfterMovingToLastSquare, newPositionAfterMovingToLastSquare );
         assertEquals(expAfterMovingOneRound, newPositionAfterMovingOneRound );
-
-
     }
 
 }
