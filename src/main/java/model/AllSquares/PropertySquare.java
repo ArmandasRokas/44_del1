b9a113bf59ec515c1b216c30a871497e5e1a7393
Armandas <armandas.rokas@gmail.com>
2018-11-26 10:25:41 +0100

    property square is updated

--- src/main/java/model/AllSquares/PropertySquare.java
+++ src/main/java/model/AllSquares/PropertySquare.java
@@ -3,7 +3,14 @@ package model.AllSquares;
 public class PropertySquare extends Square{
 
     private int price;
+    private int rentPrice;
 
+    public PropertySquare(String scenario, int price, int rentPrice){
+        super(scenario);
+        this.price = price;
+        this.rentPrice = rentPrice;
+
+    }
 
 }
 
