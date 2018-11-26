95110d29ce948fc7f652c378a9427b15c2b95950
Armandas <armandas.rokas@gmail.com>
2018-11-26 10:44:36 +0100

    property squares

--- src/main/java/model/AllSquares/PropertySquare.java
+++ src/main/java/model/AllSquares/PropertySquare.java
@@ -6,18 +6,27 @@ public class PropertySquare extends Square{
 
     private int price;
     private int rentPrice;
+    private Player owner;
+    private boolean isOwned;
 
     public PropertySquare(String scenario, int price, int rentPrice){
         super(scenario);
         this.price = price;
         this.rentPrice = rentPrice;
+        this.owner = null;
+        isOwned = false;
 
     }
 
     @Override
     public void landedOn(Player p) {
 
-
+        if(isOwned){
+           p.addToCash(-rentPrice);
+        } else {
+            p.addToCash(-price);
+            owner = p;
+        }
 
     }
 }
