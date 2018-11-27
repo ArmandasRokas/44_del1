68311d848cc639dccfffb45599e53d1412f8d48b
Armandas <armandas.rokas@gmail.com>
2018-11-27 10:36:25 +0100

    GUI is imported

--- src/main/java/ui/Abstract_UI.java
+++ src/main/java/ui/Abstract_UI.java
@@ -8,11 +8,20 @@ public abstract class Abstract_UI {
 
     private Game game;
 
+    public void setGame(Game game){
+        this.game = game;
+    }
+
+    public abstract void sayWelcome();
+
     public abstract int askForNumberOfPlayers();
 
     public abstract ArrayList<String> askForNames(int numberOfPlayers);
 
-    public void setGame(Game game){
-        this.game = game;
-    }
+    public abstract void askToTakeTurn();
+
+    public abstract void updateBoardView();
+
+
+
 }
