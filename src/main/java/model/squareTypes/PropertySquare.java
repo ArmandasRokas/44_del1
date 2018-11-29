c7105e3c5d768e27c9c928c9a2147f0b60e3c30e
Armandas <armandas.rokas@gmail.com>
2018-11-29 13:27:48 +0100

    toString in PropertySquare

--- src/main/java/model/squareTypes/PropertySquare.java
+++ src/main/java/model/squareTypes/PropertySquare.java
@@ -9,6 +9,7 @@ public class PropertySquare extends Square{
     private final String color;  //Todo enum list? https://docs.oracle.com/javase/tutorial/java/javaOO/enum.html
     private Player owner;
     private boolean isOwned;
+    private String playerAction;
 
     public PropertySquare(String scenario, int price, int rentPrice, String color){
         super(scenario);
@@ -24,13 +25,28 @@ public class PropertySquare extends Square{
     public void landedOn(Player p) {
         if(isOwned && !p.equals(owner)){
             //TODO KNA: Proper method for rent, must control if player owns a set.
-            p.addToCash(-rentPrice);
-            owner.addToCash(rentPrice);
+            buyProperty(p);
+
         } else if(!isOwned){
             p.addToCash(-price);
             this.owner = p;
             p.addOwnedSquare(this);
         }
+    }
+
+    private void buyProperty(Player p){
+        p.addToCash(-rentPrice);
+        owner.addToCash(rentPrice);
+        playerAction = p.getName() + " bought a " + super.toString() + " for " + price + "M";
+    }
+
+    private void payRent(Player p){
+        playerAction = p.getName() + " bought a " + super.toString() + " for " + price + "M";
+    }
+
+    @Override
+    public String toString() {
+        return playerAction;
     }
 }
 
