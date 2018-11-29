738bc1778bf36b0e3b1d6990d62213feee12e6f7
Armandas <armandas.rokas@gmail.com>
2018-11-29 12:46:54 +0100

    landedOn metod in ToPrison

--- src/main/java/model/squareTypes/ToPrisonSquare.java
+++ src/main/java/model/squareTypes/ToPrisonSquare.java
@@ -1,17 +1,24 @@
 package model.squareTypes;
 
+import model.Board;
 import model.Player;
 
 public class ToPrisonSquare extends Square {
+    int squareNumber;
 
-
-    public ToPrisonSquare(String scenario){
+    public ToPrisonSquare(String scenario, Board board){
         super(scenario);
+        this.squareNumber = findSquare(board);
     }
 
     //TODO skal implementeres
     @Override
     public void landedOn(Player p) {
+        p.moveToSquare(squareNumber);
+    }
 
+    private int findSquare(Board board) {
+        int index = board.findSquareByName("Prison");
+        return index;
     }
 }
