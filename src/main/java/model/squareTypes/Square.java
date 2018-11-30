08851c9b08ecb05d841d49682a40af1f05dbc39c
Kristian Andersen <kristian1711@gmail.com>
2018-11-30 11:50:40 +0100

    Comments on class: Square

--- src/main/java/model/squareTypes/Square.java
+++ src/main/java/model/squareTypes/Square.java
@@ -9,23 +9,38 @@ import model.Player;
  * Class to represent the squares on board. Is not to be created instances of, since all squares are to be unique classes.
  */
 public abstract class Square {
-    protected String scenario;
+    protected String scenario;  //Name of square, used to describe the scenario of the turn
 
     /**
+     *  Constructor of Square
      *
-     *
-     * @param scenario
+     * @param scenario  Name of the square
      */
     public Square(String scenario){
         this.scenario = scenario;
 }
 
+    /**
+     * Get method of scenario
+     *
+     * @return  Name of square
+     */
     public String getScenario(){
         return this.scenario;
     }
 
+    /**
+     * Abstract method, to act on the squares scenario
+     *
+     * @param p Reference of player who have landed on the square
+     */
     public abstract void landedOn(Player p);
 
+    /**
+     * toString method of Square
+     *
+     * @return  Message of Square
+     */
     public String toString(){
         return scenario;
     }
