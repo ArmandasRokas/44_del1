7ecc8658d88f4d4b6556813eaa7d487b4c6ecf5d
Kristian Andersen <kristian1711@gmail.com>
2018-11-30 14:09:34 +0100

    Comments on class: CCMoneyInfluence

--- src/main/java/model/chanceCardTypes/CCMoneyInfluence.java
+++ src/main/java/model/chanceCardTypes/CCMoneyInfluence.java
@@ -2,11 +2,25 @@ package model.chanceCardTypes;
 
 import model.Player;
 
+/**@author Hold 44
+ * @version 30/11-2018
+ *
+ * Defines the CCMoneyInfluence class and all of its fields and methods
+ * Class to represent the cards of the game that have influence one or more players of the game
+ */
 public class CCMoneyInfluence extends ChanceCard {
-    private int moneyInfluence;
-    private boolean actOnOtherPlayers;
-    private Player[] players;
+    private int moneyInfluence;         //Amount of the players balance there is to be influenced
+    private boolean actOnOtherPlayers;  //Boolean to determine if it affects other players
+    private Player[] players;           //List of players
 
+    /**
+     * Constructor of CCMoneyInfluence
+     *
+     * @param scenario          Scenario description of the card
+     * @param moneyInfluence    Amount of the players balance there is to be affected by the card
+     * @param actOnOtherPlayers Boolean if cards affects other players
+     * @param players           List of players in game
+     */
     public CCMoneyInfluence(String scenario, int moneyInfluence, boolean actOnOtherPlayers, Player[] players) {
         super(scenario);
         this.moneyInfluence = moneyInfluence;
@@ -14,6 +28,11 @@ public class CCMoneyInfluence extends ChanceCard {
         this.players = players;
     }
 
+    /**
+     * Affects the player(s) balance as determined by card
+     *
+     * @param currPlayer    Main player to be affected by card
+     */
     @Override
     public void actOnCard(Player currPlayer) {
         if(actOnOtherPlayers) {
