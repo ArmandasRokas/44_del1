f0c0067d0738e1a522da6c7006c3741585b3ca77
Kristian Andersen <kristian1711@gmail.com>
2018-11-30 13:34:04 +0100

    Comments on abstract class: ChanceCard Small adjustment to comments on abstract class Square

--- src/main/java/model/squareTypes/Square.java
+++ src/main/java/model/squareTypes/Square.java
@@ -3,7 +3,7 @@ package model.squareTypes;
 import model.Player;
 
 /**@author Hold 44
- * @version 08/11-2018
+ * @version 30/11-2018
  *
  * Defines the abstract Square class and all of its fields and methods
  * Class to represent the squares on board. Is not to be created instances of, since all squares are to be unique classes.
@@ -12,7 +12,7 @@ public abstract class Square {
     protected String scenario;  //Name of square, used to describe the scenario of the turn
 
     /**
-     *  Constructor of Square
+     *  Super-constructor of Square's subclasses
      *
      * @param scenario  Name of the square
      */
